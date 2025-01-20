e6a09b4c7a9d426df7166b526d37aa9fc3fc8b92
Tomás Martínez <tomas.emart@gmail.com>
2025-01-20 03:02:58 -0300

    added menu principal

--- main.cpp
+++ main.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main()
 {
+    while(true){
+        system("cls");
+        cout << "MENU PRINCIPAL" << endl;
+        cout << "1. Combate" << endl;
+        cout << "2. Salir" << endl;
+        int opcion;
+        cin >> opcion;
+        switch(opcion){
+        case 1:
+            cout << "(COMBATE)" << endl;
+            system("pause");
+            break;
+        case 2:
+            cout << "(SALIR)" << endl;
+            system("pause");
+            return 0;
+        default:
+            cout << "ERROR: OPCION INVALIDA" << endl;
+            system("pause");
+        }
+    }
 
     return 0;
 }
